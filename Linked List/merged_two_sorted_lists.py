e8feffbbec77adf397cf2ccefd71397d759c09dd
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2022-02-14 19:58:48 -0700

    add 21

---
+++ Linked List/merged_two_sorted_lists.py
@@ -0,0 +1,17 @@
+# 21. Merge Two Sorted Lists
+# Author: Xinyi Wang
+# Email: Xinyi.Wang9903@gmail.com
+
+# Question Description;
+# You are given the heads of two sorted linked lists list1 and list2.
+# Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+# Return the head of the merged linked list.
+
+# Other people solution;
+# 1. iterate those two linked list and store them in a new one 
+#    Time complexity: O(m+n)    Space complexity: O(m+n)
+# 2. Recursive
+#    if not l1 or not l2:
+#       return l1 or l2         // if one of them is null return another one 
+
+# Referance: https://leetcode.com/problems/merge-two-sorted-lists/discuss/9735/Python-solutions-(iteratively-recursively-iteratively-in-place).
